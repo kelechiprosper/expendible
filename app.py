8f8818491af4ea9bb76a85b0b117bc058b4e1824
kelechi prosper <prosperkelechi50@gmail.com>
2022-07-04 12:10:43 +0100

    few changes

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
-name = input('what is your name? ')
-favourite = input('what is your favorite color? ')
-print(name  + ' like blue')
-
+weight_lbs = input('weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.23
+print(weight_kg)
 
