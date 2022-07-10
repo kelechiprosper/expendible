ad9b564bcd102554ee44b7b0eefab8e78447dffd
kelechi prosper <prosperkelechi50@gmail.com>
2022-07-10 09:59:50 +0100

    few changes

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 
-has_good_credit = True
-has_criminal_record = True
+name = "High chief"
 
-if has_good_credit and not has_criminal_record:
-    print("Elegible for loan")
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+
+elif len(name) > 50:
+    print("Name must be maximum of 50 characters")
+
+else:
+    print("Name looks good!")
 
