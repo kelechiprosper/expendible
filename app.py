cc24408a7d15e47f2278c301e897d4774fe1099e
kelechi prosper <prosperkelechi50@gmail.com>
2022-07-10 09:01:56 +0100

    few changes

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-x = 2.6
-print(abs(-2.6))
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+    print(f"Down payment: ${down_payment}")
+
+
+
+
