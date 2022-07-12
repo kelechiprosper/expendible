444cd4e3a94f908da37af80d94e5889dac13f921
kelechi prosper <prosperkelechi50@gmail.com>
2022-07-12 06:18:34 +0100

    few changes

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-weight = int(input('weight: '))
-unit = input('(L)bs or (k)g: ')
-
-if unit.upper() == "L":
-    converted = weight * 0.45
-    print(f"You are {converted} kilos")
-else:
-    converted = weight / 0.45
-    print(f"You are {converted} pounds")
-
+i = 1
+while i <= 5:
+    print('*' * i)
+    i = i + 1
+    print("Done")
