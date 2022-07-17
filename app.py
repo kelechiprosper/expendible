27a3d7d124a98dc4bd20c9e2ff60ef6ebdcf18ce
kelechi prosper <prosperkelechi50@gmail.com>
2022-07-18 00:13:58 +0100

    few changes

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-numbers = [5, 2, 5, 2, 2]
-for x_count in numbers:
-  output = ''
-  for count in range(x_count):
-    output += 'x'
-    print(output)
+
+# Python code to remove duplicate elements
+def Remove(duplicate):
+  final_list = []
+  for num in duplicate:
+    if num not in final_list:
+      final_list.append(num)
+  return final_list
+
+# Driver Code
+duplicate = [2, 4, 10, 20, 5, 2, 20, 4]
+print(Remove(duplicate))
