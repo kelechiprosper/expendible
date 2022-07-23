1eebb41efaaf903ccdd61ae11237135206ee2821
kelechi prosper <prosperkelechi50@gmail.com>
2022-07-24 00:03:19 +0100

    few changes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def greet_user():
-    print('Hi there!')
+def greet_user(first_name, lastname_name):
+    print(f'Hi {first_name} {lastname_name}!')
     print('hello welcome to our hotel')
 
 
 print("start")
-greet_user()
+greet_user("John", "mark")
 print("finish")
